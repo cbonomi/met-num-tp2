9346aabb67c76367f0c164cc66f77eb9e288c9bc
andres-bernaus <andres.bernaus@hotmail.com>
2018-11-01 08:50:19 -0300

    subo cambios al generador

--- generador.py
+++ generador.py
@@ -13,7 +13,7 @@ def generar_graficos_knn(ejec,output,alpha,mode):
 	precision = []
 	f1 = []
 	acc = []
-	for k in [40]:#,50,100,151,201,251,301,351,401,451]:
+	for k in [3,25,45,65,86,105,125,155,175]:
 		print("k={}".format(k))
 		valor_k.append(k)
 		comm = "./{} -debug -m {} -alpha {} -k {}".format(ejec,mode,alpha,k)
@@ -27,47 +27,56 @@ def generar_graficos_knn(ejec,output,alpha,mode):
 		tn = results[2] 
 		fn = results[3]
 		tot = tp + tn + fp + fn
-		r = 100*tp / (tp + fn)
-		p = 100*tp / (tp + fp)
+		r = 100*tp / float(tp + fn)
+		p = 100*tp / float(tp + fp)
 		recall.append(r)
 		precision.append(p)
-		f1.append(2*p*r/(p+r))
-		acc.append(100*float((tp+tn))/float(tot))
+		f = 2*p*r/float(p+r)
+		f1.append(f)
+		a = 100*float((tp+tn))/float(tot)
+		acc.append(a)
+		print("K:{} ACC:{} Pre:{} Rec:{} F1:{} ".format(k,a,p,r,f))
 			
-		miny = 50
-		maxy = 80	
-		index = numpy.arange(len(valor_k))
-		bar_width = 0.35
-		plt.ylim([miny, maxy])
-		plt.bar(index,recall,bar_width, label="recall")
-		plt.bar(index+bar_width,precision,bar_width,label="Precision")
-		plt.xticks(index + bar_width / 2, valor_k)
-		#figname = "ranking_grafo_estrella_0{}.png".format(int(p*10)) 
-		figname = "{}.png".format(output)
-		plt.xlabel('Cantidad de vecinos')
-		plt.ylabel('Porcentaje')
-		plt.title('Precision y Recall de KNN')
-		plt.legend(loc='best')
-		plt.grid(True)
-		plt.savefig(figname)
-		plt.close()
 
-		#Grafico accuarcy y F1
-		index = numpy.arange(len(valor_k))
-		bar_width = 0.35
-		plt.ylim([miny, maxy])
-		plt.bar(index,acc,bar_width, label="Accuarcy")
-		plt.bar(index+bar_width,f1,bar_width,label="Media armonica (F1)")
-		plt.xticks(index + bar_width / 2, valor_k)
-		#figname = "ranking_grafo_estrella_0{}.png".format(int(p*10)) 
-		figname = "{}_acc.png".format(output)
-		plt.xlabel('Cantidad de vecinos')
-		plt.ylabel('Porcentaje')
-		plt.title('Accuarcy y media amornica de KNN')
-		plt.legend(loc='best')
-		plt.grid(True)
-		plt.savefig(figname)
-		plt.close()
+
+	# if os.path.exists("output.txt"): #optional check if file exists
+ #  		with open(file_path, 'a') as file:
+ #    file.write("\n") # could be any text, appended @ the end of file
+
+	miny = 50
+	maxy = 100	
+	index = numpy.arange(len(valor_k))
+	bar_width = 0.35
+	plt.ylim([miny, maxy])
+	plt.bar(index,recall,bar_width, label="recall")
+	plt.bar(index+bar_width,precision,bar_width,label="Precision")
+	plt.xticks(index + bar_width / 2, valor_k)
+	#figname = "ranking_grafo_estrella_0{}.png".format(int(p*10)) 
+	figname = "{}.png".format(output)
+	plt.xlabel('Cantidad de vecinos')
+	plt.ylabel('Porcentaje')
+	plt.title('Precision y Recall de KNN')
+	plt.legend(loc='best')
+	plt.grid(True)
+	plt.savefig(figname)
+	plt.close()
+
+	#Grafico accuarcy y F1
+	index = numpy.arange(len(valor_k))
+	bar_width = 0.35
+	plt.ylim([miny, maxy])
+	plt.bar(index,acc,bar_width, label="Accuarcy")
+	plt.bar(index+bar_width,f1,bar_width,label="Media armonica (F1)")
+	plt.xticks(index + bar_width / 2, valor_k)
+	#figname = "ranking_grafo_estrella_0{}.png".format(int(p*10)) 
+	figname = "{}_acc.png".format(output)
+	plt.xlabel('Cantidad de vecinos')
+	plt.ylabel('Porcentaje')
+	plt.title('Accuarcy y media amornica de KNN')
+	plt.legend(loc='best')
+	plt.grid(True)
+	plt.savefig(figname)
+	plt.close()
 
 def graph_from_data(valor_k,tp,tn,fp,fn,output):
 	tot = []
